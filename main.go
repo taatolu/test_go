76277255bedfce4d0c93e6bb994db3f77702408d
Yusaku <je.suis.fatigu@gmail.com>
2024-08-15 14:16:57 +0900

    [コミãchore: Update users.go & todos.go

--- main.go
+++ main.go
@@ -12,10 +12,36 @@ func main (){
 	log.Println("this is a Test")
 	fmt.Println(module.Db)
 
-	u := &module.User{}
-	u.Name = "Yusaku"
-	u.Email = "test@example.com"
-	u.Password = "testtest"
+	user1, err := module.GetUser(1)
+	if err != nil{
+		log.Fatalln(err)
+	}
+	fmt.Println(user1)
 
-	u.CreateUser()
+	todos, err := user1.GetTodosByUserID()
+	if err != nil{
+		log.Fatalln(err)
+	}
+	for i, v := range todos{
+		fmt.Println(i, v)
+	}
+	/*
+	user1.CreateTodo("this is second todo")
+
+	todo, err := module.GetTodo(2)
+	fmt.Println(todo)
+
+
+	err := u.CreateUser()
+	if err != nil{
+		log.Fatalln(err)
+	}
+	
+
+
+
+	user1.Password = "22testtest"
+	err = user1.UpdateUserPassword()
+	*/
+
 }
--- module/users.go
+++ module/users.go
@@ -35,3 +35,41 @@ func (u *User) CreateUser()(err error){
 	}
 	return err
 }
+
+func GetUser (userId int) (user User, err error) {
+	user = User{}
+	cmd:= `SELECT * FROM users WHERE ID = $1`
+	
+	err = Db.QueryRow(cmd,userId).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+		&user.CreateAt,
+	)
+
+	return user, err
+}
+
+
+func (u *User)UpdateUserPassword()(err error){
+	cmd := `UPDATE users SET password=$2 WHERE ID=$1`
+	newpss := Encrypt(u.Password)
+	_, err = Db.Exec(cmd, u.ID, newpss)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return err
+}
+
+
+func DeleteUser(userid int)(err error){
+	cmd := `DELETE FROM users WHERE id=$1`
+	_, err = Db.Exec(cmd,userid)
+	if err != nil{
+		log.Fatalln(err)
+	}
+	return err
+}
